aa2f29f7faa704cb96808d5ab826adee9c455dd3
Yangy <yu.yang@easyview.com.hk>
2023-07-20 17:27:35 +0800

    初步集成elasticsearch的api和使用

--- HaHa-Search/src/main/java/com/yangy/search/controller/BasicController.java
+++ HaHa-Search/src/main/java/com/yangy/search/controller/BasicController.java
@@ -1,13 +1,11 @@
 package com.yangy.search.controller;
 
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.elasticsearch.indices.GetIndexResponse;
-import com.yangy.common.bean.ResultBean;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.yangy.search.impl.EsServiceImpl;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.io.IOException;
 
 /**
@@ -19,19 +17,128 @@ import java.io.IOException;
 @RestController
 public class BasicController {
 	
-	//todo 1、存入index 2、查询index内的内容 3、更新index再查询 4、删除index
-	@Autowired
-	private ElasticsearchClient esClient;
+	@Resource
+	private EsServiceImpl esService;
 	
-	@PostMapping("/getData")
-	public ResultBean getData() throws IOException {
-		GetIndexResponse resp = esClient.indices().get(q -> q.index("_all"));
-		return ResultBean.success(resp.toString());
+	/**
+	* @Author Yangy
+	* @Description 创建索引
+	* @Date 11:46 2023/7/20
+	* @Param []
+	* @return java.lang.Object
+	**/
+	@PostMapping("/createIndex")
+	public Object createIndex() throws IOException {
+		return esService.operateOfCreateIndex();
 	}
 	
-	@PostMapping("/insertData")
-	public ResultBean insertData() throws IOException {
-//		esClient.putScript(p->p.id(""));
-		return null;
+	/**
+	* @Author Yangy
+	* @Description 删除索引
+	* @Date 11:47 2023/7/20
+	* @Param []
+	* @return java.lang.Object
+	**/
+	@PostMapping("/deleteIndex")
+	public Object deleteIndex() throws IOException {
+		return esService.operateOfDelIndex();
+	}
+	
+	
+	/**
+	* @Author Yangy
+	* @Description 创建mapping
+	* @Date 11:47 2023/7/20
+	* @Param []
+	* @return java.lang.Object
+	**/
+	@PostMapping("/createMapping")
+	public Object createMapping() throws IOException {
+		return esService.operateOfCreateMapping();
+	}
+	
+	/**
+	* @Author Yangy
+	* @Description 创建索引且创建mapping
+	* @Date 11:47 2023/7/20
+	* @Param []
+	* @return java.lang.Object
+	**/
+	@PostMapping("/createIndexAndMapping")
+	public Object createIndexAndMapping() throws IOException {
+		return esService.operateOfCreateAndAddMapping();
+	}
+	
+	/**
+	* @Author Yangy
+	* @Description 添加doc数据
+	* @Date 12:16 2023/7/20
+	* @Param []
+	* @return java.lang.Object
+	**/
+	@PostMapping("/addDoc")
+	public Object addDoc() throws IOException {
+//		return esService.operateOfAddDoc();
+		return esService.operateOfAddDocByClass();
+	}
+	
+	/**
+	* @Author Yangy
+	* @Description 查询所有文档
+	* @Date 15:09 2023/7/20
+	* @Param []
+	* @return java.lang.String
+	**/
+	@PostMapping("/getAllData")
+	public Object getData() throws IOException {
+		return esService.searchAllData();
+	}
+	
+	/**
+	* @Author Yangy
+	* @Description 查询指定doc
+	* @Date 15:14 2023/7/20
+	* @Param []
+	* @return java.lang.Object
+	**/
+	@PostMapping("/queryData")
+	public Object queryData() throws IOException {
+		return esService.getData();
+	}
+	
+	/**
+	* @Author Yangy
+	* @Description 覆盖修改整条doc
+	* @Date 16:54 2023/7/20
+	* @Param []
+	* @return java.lang.Object
+	**/
+	@PostMapping("/updateData")
+	public Object updateData() throws IOException {
+		return esService.updateData();
+	}
+	
+	/**
+	* @Author Yangy
+	* @Description 修改某条doc的部分内容
+	* @Date 16:54 2023/7/20
+	* @Param []
+	* @return java.lang.Object
+	**/
+	@PostMapping("/updateField")
+	public Object updateField() throws IOException {
+		return esService.updateDataField();
+	}
+	
+	/**
+	* @Author Yangy
+	* @Description 删除指定doc
+	* @Date 17:11 2023/7/20
+	* @Param []
+	* @return java.lang.Object
+	**/
+	@PostMapping("/deletaData")
+	public Object deletaData() throws IOException {
+		return esService.delData();
 	}
 }
--- HaHa-Search/src/main/java/com/yangy/search/impl/EsServiceImpl.java
+++ HaHa-Search/src/main/java/com/yangy/search/impl/EsServiceImpl.java
@@ -1,38 +1,78 @@
 package com.yangy.search.impl;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.mapping.TypeMapping;
+import co.elastic.clients.elasticsearch.core.DeleteResponse;
+import co.elastic.clients.elasticsearch.core.GetResponse;
+import co.elastic.clients.elasticsearch.core.IndexResponse;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.indices.CreateIndexRequest;
+import co.elastic.clients.elasticsearch.indices.CreateIndexResponse;
+import co.elastic.clients.elasticsearch.indices.PutMappingRequest;
+import co.elastic.clients.elasticsearch.indices.PutMappingResponse;
+import co.elastic.clients.json.JsonpMapper;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.json.Json;
+import jakarta.json.stream.JsonParser;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 /**
  * @Author: Yangy
  * @Date: 2023/7/10 14:47
  * @Description
+ * 参考：https://blog.csdn.net/qq_51553982/article/details/127738852
  */
 @Service
 public class EsServiceImpl {
 	
 	@Autowired
+	@Lazy
 	private ElasticsearchClient esClient;
 	
-	@PostConstruct
+	private RestClient restClient;
+	
+	private RestClientTransport transport;
+	
+	private ResourceLoader resourceLoader;
+	
+	private final static String INDEX_NAME = "haha-texts-index";
+	
+	private final static ObjectMapper JACKSON_MAPPER = new ObjectMapper();
+
+	@Autowired
+	public EsServiceImpl(ResourceLoader resourceLoader) {
+		this.resourceLoader = resourceLoader;
+	}
+
 	@Bean("esClient")
 	public ElasticsearchClient init(){
 		//创建一个低级的客户端
-		final RestClient restClient = RestClient.
-				builder(new HttpHost("localhost",9200)).build();
+		restClient = RestClient.
+				builder(new HttpHost("localhost",9200))
+				.build();
 		
 		//创建JSON对象映射器
-        final RestClientTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+        transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
         
         //创建API客户端
         final ElasticsearchClient client = new ElasticsearchClient(transport);
@@ -41,10 +81,190 @@ public class EsServiceImpl {
 	}
 	
 	@PreDestroy
-	public void shutdown(){
+	public void shutdown() throws IOException {
 		if(Objects.nonNull(esClient)){
 			esClient.shutdown();
 		}
+
+		if(Objects.nonNull(transport)){
+			transport.close();
+		}
+
+		if(Objects.nonNull(restClient)){
+			restClient.close();
+		}
 	}
 	
+	/**
+	* @Author Yangy
+	* @Description 仅创建索引
+	* @Date 18:28 2023/7/17
+	* @Param []
+	* @return java.lang.String
+	**/
+	public String operateOfCreateIndex() throws IOException {
+		boolean exist = esClient.indices().exists(query->query.index(INDEX_NAME)).value();
+		if(exist){
+			System.out.println("索引已存在");
+			return null;
+		}else {
+			CreateIndexResponse response = esClient.indices().create(builder->builder.index(INDEX_NAME));
+			return response.index();
+		}
+	}
+	
+	public boolean operateOfDelIndex() throws IOException {
+		boolean exist = esClient.indices().exists(query->query.index(INDEX_NAME)).value();
+		if(exist){
+			return esClient.indices().delete(del->del.index(INDEX_NAME)).acknowledged();
+		}else {
+			System.out.println("索引不存在");
+			return false;
+		}
+	}
+	
+	/**
+	* @Author Yangy
+	* @Description 既创建索引又创建mapping
+	* @Date 18:28 2023/7/17
+	* @Param []
+	* @return java.lang.String
+	**/
+	public boolean operateOfCreateAndAddMapping() throws IOException {
+		//方式一
+		Resource resource = resourceLoader.getResource("classpath:mapping.json");
+		JsonpMapper mapper = esClient._transport().jsonpMapper();
+		JsonParser parser = Json.createParser(resource.getInputStream());
+		CreateIndexRequest requestCreateMapping =  new CreateIndexRequest.Builder()
+                .index(INDEX_NAME)
+                .mappings(TypeMapping._DESERIALIZER.deserialize(parser, mapper))
+                .build();
+		CreateIndexResponse response = esClient.indices().create(requestCreateMapping);
+		
+		//方式二
+//		CreateIndexResponse response = esClient.indices().create(builder ->
+//                builder.settings(indexSetting -> indexSetting.numberOfReplicas("1").numberOfShards("1")).mappings(
+//                        map -> map
+//                                .properties("name", propertyBuilder -> propertyBuilder.keyword(keywordProperty -> keywordProperty))
+//                                .properties("price", propertyBuilder -> propertyBuilder.double_(doubleNumProperty -> doubleNumProperty))
+//                                .properties("des", propertyBuilder -> propertyBuilder.text(textProperty -> textProperty.analyzer("ik_smart").searchAnalyzer("ik_smart")))
+//                ).index("produces")
+//
+//        );
+		
+		return response.acknowledged();
+	}
+	
+	public boolean operateOfCreateMapping() throws IOException {
+		Resource resource = resourceLoader.getResource("classpath:mapping.json");
+        
+        // 定义 Mapping
+        PutMappingRequest mappingRequest = new PutMappingRequest.Builder()
+				.index(INDEX_NAME)
+				.withJson(resource.getInputStream())
+                .build();
+
+        PutMappingResponse mappingResponse = esClient.indices().putMapping(mappingRequest);
+		
+		return mappingResponse.acknowledged();
+	}
+	
+	/**
+	* @Author Yangy
+	* @Description 通过json文件导入doc数据
+	* @Date 14:10 2023/7/20
+	* @Param []
+	* @return boolean
+	**/
+	public boolean operateOfAddDocByFile(){
+		Resource docJsonResc = resourceLoader.getResource("classpath:doc1.json");
+		ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			Map<String, Object> dataMap = objectMapper.readValue(docJsonResc.getInputStream(), Map.class);
+			esClient.index(builder -> builder.index(INDEX_NAME).
+					document(dataMap));
+			
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+	}
+	
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor
+	class StructInfo {
+		public String zh_CN;
+		public String en_US;
+	}
+	
+	/**
+	* @Author Yangy
+	* @Description 通过实体类或map形式新增doc
+	* @Date 14:22 2023/7/20
+	* @Param []
+	* @return long
+	**/
+	public long operateOfAddDocByClass() throws IOException {
+		Map<String,Object> dataMap = new HashMap<>();
+		dataMap.put("id","1");
+		dataMap.put("contents",new StructInfo("一条测试内容","test content"));
+		dataMap.put("titles",new StructInfo("测试标题","test title"));
+		dataMap.put("channels","app");
+		
+		IndexResponse response = esClient.index(builder -> 
+				builder.index(INDEX_NAME).id(dataMap.get("id").toString())
+				.document(dataMap));
+		return response.version();
+	}
+	
+	public String searchAllData() throws IOException {
+		SearchResponse<Map> response = esClient.search(query->query.index(INDEX_NAME),Map.class);
+		List<Map> mapList = new ArrayList<>();
+		response.hits().hits().stream()
+				.forEach(r->{
+					mapList.add(r.source());
+				});
+		return JACKSON_MAPPER.writeValueAsString(mapList);
+	}
+	
+	public String getData() throws IOException {
+		GetResponse<Map> response = esClient.get(query->query.index(INDEX_NAME).id("1"),Map.class);
+		Map data = response.source();
+		return JACKSON_MAPPER.writeValueAsString(data);
+	}
+	
+	public Object updateData() throws IOException {
+		Map<String,Object> dataMap = new HashMap<>();
+		dataMap.put("id","1");
+		dataMap.put("contents",new StructInfo("一条测试内容 V2","test content V2"));
+		dataMap.put("titles",new StructInfo("测试标题 V2","test title V2"));
+		dataMap.put("channels","app");
+		
+		//全覆盖
+		return esClient.update(builder -> builder.
+				index(INDEX_NAME).id("1").doc(dataMap), Map.class)
+				.shards().successful().intValue();
+	}
+	
+	
+	public Object updateDataField() throws IOException {
+		Map<String,Object> dataMap = new HashMap<>();
+		dataMap.put("id","1");
+		dataMap.put("contents",new StructInfo("一条测试内容 V3","test content V3"));
+		dataMap.put("titles",new StructInfo("测试标题 V3","test title V3"));
+		dataMap.put("channels","app");
+		
+		//修改部分字段
+		return esClient.update(builder -> builder.
+				index(INDEX_NAME).id("1").doc(dataMap).docAsUpsert(true), Map.class)
+				.shards().successful().intValue();
+	}
+	
+	public Object delData() throws IOException {
+		DeleteResponse response = esClient.delete(builder -> builder.index(INDEX_NAME).id("1"));
+		return response.shards().successful().intValue();
+	}
 }
